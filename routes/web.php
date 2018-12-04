c004724df0dbc0992680f9a0a97d55c3cd178096
bibbary <bibbary@gmail.com>
2018-12-04 01:25:35 +0100

    titre et nav....ok insertion logo..ok

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/causes', function () {
+    return view('causes');
+});
+Route::get('/event', function () {
+    return view('event');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/gallery', function () {
+    return view('gallery');
 });
